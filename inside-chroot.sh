8a37a9c8c51e43e90f691cfd7b4afde1c6bb6337
Matt Olenik <olenikm@gmail.com>
2018-09-15 02:14:00 -0700

    Remove incorrect locale setting

--- inside-chroot.sh
+++ inside-chroot.sh
@@ -82,7 +82,6 @@ setup_clock() {
 set_locale() {
   echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
   echo 'LANG=en_US.UTF-8' > /etc/locale.conf
-  echo 'LC_ALL=en_US.UTF-8' > /etc/locale.conf
   locale-gen
 }
 
