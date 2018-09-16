85250d4fc94f7d2e7e416a4b9f31c499324c64e0
Matt Olenik <olenikm@gmail.com>
2018-09-15 19:17:34 -0700

    Improve robustness and use env var for auto approve

--- common.sh
+++ common.sh
@@ -1,5 +1,4 @@
 # shellcheck shell=bash
-
 ##
 # Prompt the user and wait for an answer. An optional default value
 # can be returned when the user skips the question by pressing ENTER.
@@ -10,58 +9,46 @@
 #      added automatically.
 # $3 - Options for the prompt, as a space-separated list. Use * for freeform
 #      input. Options are automatically lowercase only.
-# $4 - Default value, or empty string if unset
+# $4 - Default value, optional
 ##
 ask() {
-  local out_var="$1"
   local question="$2"
-  # Trim and reduce whitespace and replace spaces with pipes
   local options="${3:-*}"
   local default="${4:-}"
-  local confirm
-  local result
-  get_named_arg confirm "confirm" '' "${@:5}"
-  echo $confirm
+  local answer
+
+  # If ASK_AUTO_APPROVE is set, just return the default.
+  if [[ -n $ASK_AUTO_APPROVE ]]; then
+    read -r "$1" <<< "$default"
+  fi
 
   # A nicely formatted option string in the style of: (yes/no/CANCEL)
   # The default option, if any, will be in uppercase.
   local options_string="${options+(${options[@]// /\/}) }"
-  if [[ -n "$default" ]] && [[ $options_string != *"$default"* ]]; then
-    echo "Default value does not appear in the options list" && return 3
+  if [[ -n "$default" ]]; then
+    if [[ $options_string != *"$default"* ]]; then
+      echo "Default value does not appear in the options list" && return 3
+    fi
+
     # Make the default option appear in uppercase
     options_string="${options_string/$default/${default^^}}"
   fi
   while true; do
-    read -rp "${question} $options_string" result
-    if [[ -z $result ]] && [[ -z $default ]]; then
-      return
-    fi
+    read -rp "$question $options_string" answer
+    answer="${answer:-$default}"
     if [[ $options == "*" ]]; then
       # Populate the user-passed in variable
-      read -r "$out_var" <<< "$result"
+      read -r "$1" <<< "$answer"
+      return
+    fi
+    # Trim and collapse whitespace and convert to lowercase
+    options="$(printf %s "$options" | xargs echo -n | awk '{print tolower($0)}')"
+    local opt_pattern='^('"${options// /|}"')$'
+    if [[ $answer =~ $opt_pattern ]]; then
+      read -r "$1" <<< "$answer"
       return
     else
-      # Trim and collapse whitespace and convert to lowercase
-      options="$(printf %s "$options" | xargs echo -n | awk '{print tolower($0)}')"
-      if [[ $result =~ ^(${options// /|})$ ]]; then
-	read -r "$out_var" <<< "$result"
-	return
-      else
-	echo "ERROR: Invalid option, must be one of: ${options// /,}"
-      fi
+      echo "ERROR: Invalid option, must be one of: ${options// /,}"
     fi
   done
-}
-
-get_named_arg() {
-  local out_var="$1"
-  local key="$2"
-  local default="${3:-}"
-  shift 3
-  for pair in "$@"; do
-    if [[ $pair =~ ^@([^=]+)=([^=]*)$ ]] && [[ ${BASH_REMATCH[1]} == "$key" ]]; then
-      read -r "$out_var" <<< "${BASH_REMATCH[2]}" && return
-    fi
-  done
-  read -r "$out_var" <<< "$default"
 }
