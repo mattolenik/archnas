393c8092929c26414db9b03fb23300d224d1e1bf
Matt Olenik <olenikm@gmail.com>
2025-07-01 11:36:09 -0700

    Fix frigate

--- archnas/fs/usr/lib/frigate/start.sh
+++ archnas/fs/usr/lib/frigate/start.sh
@@ -2,6 +2,7 @@
 set -exuo pipefail
 
 mkdir -p "$MEDIA_DIR"
+mkdir -p "$CONFIG_DIR"
 
 tepid "$CONFIG_FILE" > "$RUNTIME_CONFIG"
 
@@ -12,12 +13,14 @@ podman run \
     --sdnotify=conmon \
     --replace \
     -d \
+    --privileged \
     --name frigate \
     --mount type=tmpfs,target=/tmp/cache,tmpfs-size=1000000000 \
     --device /dev/bus/usb:/dev/bus/usb \
     --device /dev/dri/renderD128 \
     --shm-size="$SHM_SIZE" \
     -v "$MEDIA_DIR:/media/frigate" \
+    -v "$CONFIG_DIR:/config" \
     -v "$RUNTIME_CONFIG:/config/config.yml" \
     -v /etc/localtime:/etc/localtime:ro \
     -p 8554:8554 \
