c2b872b2694107442a698c7adb319d4f9f3f8da9
Matt Olenik <olenikm@gmail.com>
2025-09-27 15:06:52 -0700

    cgo unnecessary for tiny tool

--- archnas/steps/05.tepid.sh
+++ archnas/steps/05.tepid.sh
@@ -1,1 +1,1 @@
-go build -o /usr/bin/tepid /usr/src/tepid/main.go # templating tool to assist with inserting systemd creds into config files
+CGO_ENABLED=0 go build -o /usr/bin/tepid /usr/src/tepid/main.go # templating tool to assist with inserting systemd creds into config files
