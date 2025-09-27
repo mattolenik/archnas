6525bcbbc7d0a87988725b21d5057c958e63f203
Matt Olenik <olenikm@gmail.com>
2025-09-27 15:04:38 -0700

    consolidate

--- archnas/steps/03.setup-users.sh
+++ archnas/steps/03.setup-users.sh
@@ -1,9 +1,16 @@
 echo "Setting up user $USER_NAME"
+
 useradd -m -G adm,log,sys,uucp,wheel -s "$(command -v zsh)" "$USER_NAME"
 chpasswd <<<"$USER_NAME:$PASSWORD"
-add_ssh_key_from_github "$GITHUB_USERNAME"
+
+echo "Allowing SSH for GitHub user $GITHUB_USERNAME"
+mkdir -m 0700 "$USER_HOME/.ssh"
+curl -sS "https://github.com/$GITHUB_USERNAME.keys" >>"$USER_HOME/.ssh/authorized_keys"
+chmod 600 "$USER_HOME/.ssh/authorized_keys"
+
 # shellcheck disable=SC2016
 echo 'command -v starship &>/dev/null && eval "$(starship init bash)"' >>"$USER_HOME/.bashrc"
 # shellcheck disable=SC2016
 echo 'command -v starship &>/dev/null && eval "$(starship init zsh)"' >>"$USER_HOME/.zshrc"
+
 chown -c -R "$USER_NAME:$USER_NAME" "$USER_HOME"
--- archnas/steps/11.ssh.sh
+++
@@ -1,4 +0,0 @@
-echo "Allowing SSH for GitHub user $1"
-mkdir -m 0700 "$USER_HOME/.ssh"
-curl -sS "https://github.com/$1.keys" >>"$USER_HOME/.ssh/authorized_keys"
-chmod 600 "$USER_HOME/.ssh/authorized_keys"
